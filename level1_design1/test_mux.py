e0fb1c19d2c41c087c99a8d4afc2002634ad565c
HARSHIL156 <soniharshil15@gmail.com>
2022-07-29 10:59:18 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -14,29 +14,9 @@ async def mux_basic_test(dut):
     S = 0
 
     # input driving
-    dut.a.value = A
-    dut.b.value = B
+    dut.inp0.value = A
+    dut.sel.value = S
 
     await Timer(2, units='ns')
     
-    assert dut.sum.value == A+B, "Adder result is incorrect: {A} + {B} != {SUM}, expected value={EXP}".format(
-            A=int(dut.a.value), B=int(dut.b.value), SUM=int(dut.sum.value), EXP=A+B)
-
-
-@cocotb.test()
-async def mux_randomised_test(dut):
-    """Test for adding 2 random numbers multiple times"""
-
-    for i in range(5):
-
-        A = random.randint(0, 15)
-        B = random.randint(0, 15)
-
-        dut.a.value = A
-        dut.b.value = B
-
-        await Timer(2, units='ns')
-        
-        dut._log.info(f'A={A:05} B={B:05} model={A+B:05} DUT={int(dut.sum.value):05}')
-        assert dut.sum.value == A+B, "Randomised harshil test failed with: {A} + {B} = {SUM}".format(
-            A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
+    assert dut.out.value == A, "Mux result is incorrect for input {A} and sel {S}. because {OUT}!={out}".format( A=int(dut.inp0.value), S=int(dut.sel.value),OUT=int(dut.out.value), out=int(dut.inp0.value))
