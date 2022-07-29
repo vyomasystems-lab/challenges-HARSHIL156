8936c5b3033621de7638a1e647f9dfc5745665ab
HARSHIL156 <soniharshil15@gmail.com>
2022-07-29 11:45:48 +0000

    updated

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -13,14 +13,16 @@ from cocotb.triggers import RisingEdge, FallingEdge
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
-
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
-
+    
     # reset
     dut.reset.value = 1
-    await FallingEdge(dut.clk)  
+    await FallingEdge(dut.clk) 
+    dut.inp_bit.value=1
+    dut.inp_bit.value=0
+    dut.inp_bit.value=1
+    dut.inp_bit.value=1
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+    assert dut.SEQ_1011.value == 1, "FSM sequence for {Current_state}=={SEQ1011} but {Seq_seen} != 1".format( Current_state=int(dut.current_state.value),SEQ1011=int(dut.SEQ_1011.value),Seq_seen=int(dut.seq_seen.value))
