9ec9a0b8ca0a8e3769672fa37f3525bf26139cb2
HARSHIL156 <soniharshil15@gmail.com>
2022-08-01 06:02:56 +0000

    updated

--- vyoma.sh
+++ vyoma.sh
@@ -11,6 +11,6 @@ cp -r /workspace/.pyenv_mirror/user/3.8.13/lib/python3.8/site-packages/cocotb*  
 cp -r /workspace/.pyenv_mirror/user/3.8.13/lib/python3.8/site-packages/pygpi /home/gitpod/.pyenv/versions/3.8.13/lib/python3.8/site-packages/
 rm -rf core.*
 clear
-echo "****** UpTickPro (Evaluation Version) 1.0.0 *******"
+echo "** UpTickPro (Evaluation Version) 1.0.0 ***"
 echo "Copyright (c) 2022, Vyoma Systems Private Limited"
 echo "All Rights Reserved."
