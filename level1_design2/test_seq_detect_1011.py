064006fdb040359143c55b14c0cfe0d5fd5a2ca3
HARSHIL156 <soniharshil15@gmail.com>
2022-08-01 05:59:32 +0000

    updated

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -16,13 +16,18 @@ async def test_seq_bug1(dut):
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
     
-    # reset
+      
+      # reset
     dut.reset.value = 1
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
     await FallingEdge(dut.clk) 
     dut.inp_bit.value=1
+    await FallingEdge(dut.clk) 
     dut.inp_bit.value=0
+    await FallingEdge(dut.clk) 
     dut.inp_bit.value=1
+    await FallingEdge(dut.clk) 
     dut.inp_bit.value=1
-    dut.reset.value = 0
-    await FallingEdge(dut.clk)
-    assert dut.SEQ_1011.value == 1, "FSM sequence for {Current_state}=={SEQ1011} but {Seq_seen} != 1".format( Current_state=int(dut.current_state.value),SEQ1011=int(dut.SEQ_1011.value),Seq_seen=int(dut.seq_seen.value))
+    await FallingEdge(dut.clk) 
+    assert dut.seq_seen.value == 1, "FSM sequence for {Current_state}(current state)== {SEQ1011}(detect_seq_state) but  {Seq_seen}(seq_seen) != 1".format( Current_state=int(dut.current_state.value),SEQ1011=dut.SEQ_1011.value,Seq_seen=dut.seq_seen.value)
